2084169faf90c143ca48872575fdffdf5f308bd7
Vishesh Arora <>
2022-09-14 22:36:26 +0800

    BVO-47 create template for segmap generation

--- src/composite_nodes.py
+++ src/composite_nodes.py
@@ -1,7 +1,11 @@
 import bpy
+import time
 
 ## IMportant - CHANGE object's pass index and set render engine to cycles
 
+
+bpy.context.scene.render.engine = 'CYCLES'
+bpy.context.scene.view_layers["ViewLayer"].use_pass_object_index = True
 bpy.context.scene.use_nodes = True
 tree = bpy.context.scene.node_tree
 
@@ -21,12 +25,40 @@ file_node.location = 500, -300
 
 math_node = tree.nodes.new('CompositorNodeMath')
 math_node.operation = 'DIVIDE'
-math_node.inputs[1].default_value = 255
 math_node.location = 300, -200
 
 # link nodes
 links = tree.links
 link = links.new(render_node.outputs[0], comp_node.inputs[0])
 link = links.new(render_node.outputs[0], file_node.inputs[0])
-link = links.new(render_node.outputs[3], math_node.inputs[0])
-link = links.new(math_node.outputs[0], file_node.inputs[0]) # Change between math_node and render_node
+link = links.new(render_node.outputs["IndexOB"], math_node.inputs[0])
+
+render_segmentation = True
+render_path = "/Users/vishesh/Desktop/DSTA/blender/blender-synthetics/"
+
+if render_segmentation:
+    math_node.inputs[1].default_value = 65535 # Change for 8 bit png
+    link = links.new(math_node.outputs[0], file_node.inputs[0])
+    file_node.format.color_mode = 'BW'
+    file_node.format.color_depth = '16'
+    file_node.format.file_format = 'PNG'
+    file_node.base_path = render_path
+    file_node.file_slots[0].path = "test"
+else:
+    math_node.inputs[1].default_value = 65535 # Change for 8 bit png
+    link = links.new(math_node.outputs[0], file_node.inputs[0])
+    file_node.format.color_mode = 'BW'
+    file_node.format.color_depth = '16'
+    file_node.format.file_format = 'PNG'
+    file_node.base_path = render_path
+    file_node.file_slots[0].path = "test"
+
+
+for i in range(3):
+    file_node.file_slots[0].path = f"test{i}"
+    bpy.ops.render.render(write_still=True)
+
+
+for i in range(4,6):
+    file_node.file_slots[0].path = f"test{i}"
+    bpy.ops.render.render(write_still=True)
