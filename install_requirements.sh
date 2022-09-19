ecef65ac06f2e0dba38fe18e56f704e4b9369a1b
wish2023 <vishesharora@gmail.com>
2022-09-19 16:23:53 +0800

    BVO-56 create basic shell executable

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,3 +1,4 @@
-BLENDER_PATH='dummy/path'
-echo $1$BLENDER_PATH
-# pip install -r blender_requirements.txt -t $1$BLENDER_PATH
+BLENDER_PATH='3.2/python/lib/python3.10/site-packages'
+# echo $1$BLENDER_PATH
+pip install -r blender_requirements.txt -t $1$BLENDER_PATH
+pip install -r requirements.txt
