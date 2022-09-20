bfe1dae867e42c5b828c0cd2d431bc20a60ecb3a
wish2023 <vishesharora@gmail.com>
2022-09-20 12:57:20 +0800

    BVO-54 fill polygons over overlapping vehicles

--- src/create_labels.py
+++ src/create_labels.py
@@ -81,6 +81,8 @@ def intersect(x1, y1, x2, y2, x3, y3, x4, y4):
 
 for img_name in os.listdir(img_path):
 
+    overlapping = set() # instances indices which overlap
+
     ann = {"cat_id": [], "xc": [], "yc": [], "w": [], "h": [], 
         "obb1x": [], "obb1y": [], "obb2x": [], "obb2y": [], 
         "obb3x": [], "obb3y": [], "obb4x": [], "obb4y": []}
@@ -113,21 +115,55 @@ for img_name in os.listdir(img_path):
         points = cv2.findNonZero((seg_map == inst).astype(int))
         if points is None:
             continue
-        x, y, w, h = cv2.boundingRect(points)
+        x_bb, y_bb, w, h = cv2.boundingRect(points)
 
-        if occlusion_aware == False and (x == 0 or y == 0 or x+w == img.shape[1] or y+h == img.shape[0]): # bounding box on edge, object partially not in frame
+        if occlusion_aware == False and (x_bb == 0 or y_bb == 0 or x_bb+w == img.shape[1] or y_bb+h == img.shape[0]): # bounding box on edge, object partially not in frame
             continue # re-adjust bb if occlusion_aware, so that OBBs don't extend image
 
         obb = cv2.minAreaRect(points)
         obb_points = cv2.boxPoints(obb).astype(int)
 
-        if view_annotations:
-            img_bb = cv2.rectangle(img_bb, (x, y), (x+w, y+h), color=colors[cat_id], thickness=2)
+        for i in range(obb_points.shape[0]): # check every segment
+            x1, y1, x2, y2 = obb_points[i][0], obb_points[i][1], \
+                                obb_points[(i+1) % obb_points.shape[0]][0], obb_points[(i+1) % obb_points.shape[0]][1]
+            
+            for j in range(len(ann["xc"])): # number of boxes in img so far
+                obb_points2 = np.array([[ann["obb1x"][j], ann["obb1y"][j]], 
+                                        [ann["obb2x"][j], ann["obb2y"][j]], 
+                                        [ann["obb3x"][j], ann["obb3y"][j]], 
+                                        [ann["obb4x"][j], ann["obb4y"][j]]])
+
+                
+                w2, h2 = round(ann["w"][j]*img_w), round(ann["h"][j]*img_h)
+                x_bb2, y_bb2 = round((ann["xc"][j] * img_w) - w2/2), round((ann["yc"][j] * img_h) - h2/2)
+
+
+                for k in range(obb_points2.shape[0]):
+                    x3, y3, x4, y4 = obb_points2[k][0], obb_points2[k][1], \
+                                obb_points2[(k+1) % obb_points2.shape[0]][0], obb_points2[(k+1) % obb_points2.shape[0]][1]
+
+                    
+                    if intersect(x1, y1, x2, y2, x3, y3, x4, y4):
+                        overlapping.add(j)
+                        overlapping.add(len(ann["xc"]))
+
+                        img_obb = cv2.polylines(img_obb, [obb_points], isClosed=True, color=(0,0,255), thickness=3)
+                        img_obb = cv2.polylines(img_obb, [obb_points2], isClosed=True, color=(0,0,255), thickness=3)
+                        img_bb = cv2.rectangle(img_bb, (x_bb, y_bb), (x_bb+w, y_bb+h), color=(0,0,255), thickness=3)
+                        img_bb = cv2.rectangle(img_bb, (x_bb2, y_bb2), (x_bb2+w2, y_bb2+h2), color=(0,0,255), thickness=3)
+                        img = cv2.fillPoly(img, [obb_points], color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+                        img = cv2.fillPoly(img, [obb_points2], color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+
+                        continue
+
+
+        if view_annotations and len(ann["xc"]) not in overlapping: # if current instance isn't overlapping
+            img_bb = cv2.rectangle(img_bb, (x_bb, y_bb), (x_bb+w, y_bb+h), color=colors[cat_id], thickness=2)
             img_obb = cv2.polylines(img_obb, [obb_points], isClosed=True, color=colors[cat_id], thickness=2)
 
         ann["cat_id"].append(cat_id) 
-        ann["xc"].append((x + w/2) / img_w)
-        ann["yc"].append((y + h/2) / img_h)
+        ann["xc"].append((x_bb + w/2) / img_w)
+        ann["yc"].append((y_bb + h/2) / img_h)
         ann["w"].append(w / img_w)
         ann["h"].append(h / img_h)
 
@@ -141,13 +177,16 @@ for img_name in os.listdir(img_path):
         ann["obb4y"].append(obb_points[3][1])
 
 
+
     df = pd.DataFrame.from_dict(ann)
+    df = df.drop(list(overlapping))
     yolo_col = ["cat_id", "xc", "yc", "w", "h"]
     np.savetxt(os.path.join(yolo_labels_path, img_name[:-4] + ".txt"), df[yolo_col], delimiter=' ', fmt=['%d', '%.4f', '%.4f', '%.4f', '%.4f'])
 
     obb_col = ["cat_id", "obb1x", "obb1y", "obb2x", "obb2y", "obb3x", "obb3y", "obb4x", "obb4y"]
     np.savetxt(os.path.join(obb_labels_path, img_name[:-4] + ".txt"), df[obb_col], delimiter=' ', fmt=['%d', '%d', '%d', '%d', '%d', '%d', '%d', '%d', '%d'])
     
+    cv2.imwrite(os.path.join(img_path, img_name), img)
     if view_annotations:
         cv2.imwrite(os.path.join(yolo_annotated_path, img_name), img_bb)
         cv2.imwrite(os.path.join(obb_annotated_path, img_name), img_obb)
