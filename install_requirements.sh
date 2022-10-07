08eb95ed5ef07e766853fdc53326e775bd3b64c1
Vishesh Arora <>
2022-10-07 11:27:35 +0800

    Install blender libraries

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-BLENDER_PATH='/python/lib/python3.10/site-packages'
-# echo $1$BLENDER_PATH
-pip install -r blender_requirements.txt -t $1$BLENDER_PATH
+# Ensure pip: equl to /blender-path/3.xx/python/bin/python3.10 -m ensurepip
+blender -b --python-expr "from subprocess import sys,call;call([sys.executable,'-m','ensurepip'])"
+# Update pip toolchain
+blender -b --python-expr "from subprocess import sys,call;call([sys.executable]+'-m pip install -U pip setuptools wheel'.split())"
+# pip install
+blender -b --python-expr "from subprocess import sys,call;call([sys.executable]+'-m pip install -U bpycv'.split());call([sys.executable]+'-m pip install -U PyYAML'.split());call([sys.executable]+'-m pip install -U glob2'.split())"
+
 pip install -r requirements.txt
